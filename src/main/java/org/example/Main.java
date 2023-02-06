8d3a7233c7db942714a42f0b71c37c214745e77d
Igor Shmakov <workshmakov@mail.ru>
2023-02-06 12:55:54 +0300

    dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.io.File;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        String folderPath = "C:/donload/Total War - ROME II by xatab";
+        File file = new File(folderPath);
+        System.out.println(getFolderSize(file));
+    }
 
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
